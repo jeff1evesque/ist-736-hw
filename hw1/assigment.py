3d342f3ac2a020618db470e81ea1e53e542486f3
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-07 16:00:52 -0400

    #5: assignment.py, implement vador

--- hw1/assigment.py
+++ hw1/assigment.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python
 
+#
+# this files requires nltk to be installed:
+#
+#     pip install nltk
+#
+
 import time
 import csv
 import numpy as np
 from pathlib import Path
 import pandas as pd
+from nltk import tokenize, download
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+download('vader_lexicon')
 
+
+def vador_analysis(fp='{}/data/sample-sentiment.csv'.format(
+    Path(__file__).resolve().parents[1]
+)):
+    sentences = pd.read_csv(fp)['SentimentText']
+    sid = SentimentIntensityAnalyzer()
+
+    result = []
+    for i, s in enumerate(sentences):
+        ss = sid.polarity_scores(s)
+
+        scores = []
+        for k in sorted(ss):
+            scores.append({k: ss[k]})
+
+        result.append({i: scores})
+
+    return({'sent': sentences, 'result': result})
 
 def time_df(fp='{}/data/sample-sentiment.csv'.format(
     Path(__file__).resolve().parents[1]
@@ -40,3 +67,6 @@ if __name__ == '__main__':
     tdf = time_df()
     print('panda upload time: {}'.format(tdf['pd_time']))
     print('numpy upload time: {}'.format(tdf['np_time']))
+
+    va = vador_analysis()
+    [print('{}\n{}\n\n'.format(x, va['result'][i])) for i,x in enumerate(va['sent'])]
