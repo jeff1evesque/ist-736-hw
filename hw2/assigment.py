17acdf96b6101972ad959fdd475d35eec7367729
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-13 16:22:57 -0400

    #13: assignment.py, generate pos

--- hw2/assigment.py
+++ hw2/assigment.py
@@ -13,8 +13,7 @@ import csv
 import numpy as np
 from pathlib import Path
 import pandas as pd
-from nltk import tokenize, download
-from nltk.tag import StanfordPOSTagger
+from nltk import tokenize, download, pos_tag
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
@@ -52,7 +51,7 @@ class Ensemble():
 
         # pos dataframe
         self.pos = self.data
-        self.pos[self.key_value] = self.get_pos(self.data[self.key_value])
+        self.pos[self.key_text] = self.get_pos(self.data[self.key_text])
 
     def split(self, test_size=0.25):
         '''
@@ -62,20 +61,19 @@ class Ensemble():
         '''
 
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.data[key_text],
-            self.data[key_class],
+            self.data[self.key_text],
+            self.data[self.key_class],
             test_size=test_size
         )
 
-    def get_pos(self, l, tagger='english-bidirectional-distsim.tagger'):
+    def get_pos(self, l):
         '''
 
         apply stanford pos tagger to supplied list.
 
         '''
 
-        st = StanfordPOSTagger(tagger)
-        return([st.tag(x) for x in l])
+        return([pos_tag(x) for x in l])
 
     def vectorize(self):
         '''
@@ -85,12 +83,12 @@ class Ensemble():
         '''
 
         # bag of words: with 'english' stopwords
-        count_vect = CountVectorizer(stop_words='english')
-        bow = count_vect.fit_transform(self.X_train)
+        self.count_vect = CountVectorizer(stop_words='english')
+        bow = self.count_vect.fit_transform(self.X_train)
 
         # tfidf weighting
-        tfidf_transformer = TfidfTransformer()
-        self.X_train_tfidf = tfidf_transformer.fit_transform(bow)
+        self.tfidf_transformer = TfidfTransformer()
+        self.X_train_tfidf = self.tfidf_transformer.fit_transform(bow)
 
     def nb_model(self):
         '''
@@ -105,21 +103,23 @@ class Ensemble():
         # predict
         predictions = []
         for item in list(self.X_test):
-            prediction = count_vect.transform([item])
+            prediction = self.count_vect.transform([item])
             predictions.append(
-                self.clf.predict(tfidf_transformer.fit_transform(prediction))
+                self.clf.predict(self.tfidf_transformer.fit_transform(prediction))
             )
 
         # fit model
         return({
-            'model': clf,
+            'model': self.clf,
             'actual': self.y_test,
             'predicted': predictions
         })
 
 if __name__ == '__main__':
+    model = Ensemble()
+
     # naive bayes prediction
-    model = nb_model()
+    model = model.nb_model()
     skplt.metrics.plot_confusion_matrix(
         model['actual'],
         model['predicted']
