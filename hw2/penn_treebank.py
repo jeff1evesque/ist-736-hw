4d7dad0cddc5188f2c80424771467c0553d0ed5d
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-13 22:29:49 -0400

    #13: penn_treebank.py, convert function to variable

--- hw2/penn_treebank.py
+++ hw2/penn_treebank.py
@@ -6,48 +6,41 @@
 #     https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
 #
 
-def penn_scale():
-    '''
-
-    categorization of penn-tree
-
-    '''
-
-    return {
-        'CC': 1,
-        'CD': 2,
-        'DT': 3,
-        'EX': 4,
-        'FW': 5,
-        'IN': 6,
-        'JJ': 7,
-        'JJR': 8,
-        'JJS': 9,
-        'LS': 10,
-        'MD': 11,
-        'NN': 12,
-        'NNS': 13,
-        'NNP': 14,
-        'NNPS': 15,
-        'PDT': 16,
-        'POS': 17,
-        'PRP': 18,
-        'PRP$': 19,
-        'RB': 20,
-        'RBR': 21,
-        'RBS': 22,
-        'RP': 23,
-        'SYM': 24,
-        'TO': 25,
-        'UH': 26,
-        'VB': 27,
-        'VBD': 28,
-        'VBG': 29,
-        'VBN': 30,
-        'VBP': 31,
-        'VBZ': 32,
-        'WDT': 33,
-        'WP': 34,
-        'WP$': 35,
-        'WRB': 36
-    }
+penn_scale = {
+    'CC': 1,
+    'CD': 2,
+    'DT': 3,
+    'EX': 4,
+    'FW': 5,
+    'IN': 6,
+    'JJ': 7,
+    'JJR': 8,
+    'JJS': 9,
+    'LS': 10,
+    'MD': 11,
+    'NN': 12,
+    'NNS': 13,
+    'NNP': 14,
+    'NNPS': 15,
+    'PDT': 16,
+    'POS': 17,
+    'PRP': 18,
+    'PRP$': 19,
+    'RB': 20,
+    'RBR': 21,
+    'RBS': 22,
+    'RP': 23,
+    'SYM': 24,
+    'TO': 25,
+    'UH': 26,
+    'VB': 27,
+    'VBD': 28,
+    'VBG': 29,
+    'VBN': 30,
+    'VBP': 31,
+    'VBZ': 32,
+    'WDT': 33,
+    'WP': 34,
+    'WP$': 35,
+    'WRB': 36
+}
