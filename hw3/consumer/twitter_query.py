8fc3e2491fb0e917044f31c29768014fc57986f5
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-21 09:26:52 -0400

    #29: twitter_query.py, remove newline

--- hw3/consumer/twitter_query.py
+++ hw3/consumer/twitter_query.py
@@ -31,7 +31,6 @@ class TwitterQuery():
             d = d.get(k, None)
             if d is None:
                 return(None)
-
         return(d.encode('utf-8'))
 
     def get_dict_path(self, d):
