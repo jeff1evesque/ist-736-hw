34251b0f517c2d28705f6cc7391b5a1219bc674b
jeffrey levesque <jeff1evesque@yahoo.com>
2019-04-21 10:35:53 -0400

    #31: assignment.py, remove urls from 'text' column

--- hw3/assignment.py
+++ hw3/assignment.py
@@ -25,6 +25,11 @@ if Path(csv_cnn).is_file():
     df_cnn = pd.read_csv(csv_cnn)
 else:
     df_cnn = q.query_user('cnn')
+    df_cnn['text'] = df_cnn['text'].str.replace(
+        'http\S+|www.\S+',
+        '',
+        case=False
+    )
     df_cnn.to_csv(csv_cnn)
 
 # tweets: foxnews
@@ -32,6 +37,11 @@ if Path(csv_foxnews).is_file():
     df_foxnews = pd.read_csv(csv_foxnews)
 else:
     df_foxnews = q.query_user('FoxNews')
+    df_foxnews['text'] = df_foxnews['text'].str.replace(
+        'http\S+|www.\S+',
+        '',
+        case=False
+    )
     df_foxnews.to_csv(csv_foxnews)
 
 # combine dataframes
@@ -40,6 +50,6 @@ df = df_cnn.append(df_foxnews)
 # generate wordcloud
 if not os.path.exists('viz'):
     os.makedirs('viz')
-word_cloud(df_cnn, filename='viz/wc_cnn.png')
-word_cloud(df_foxnews, filename='viz/wc_foxnews.png')
-word_cloud(df, filename='viz/wc_cnn_foxnews.png')
+word_cloud(df_cnn['text'], filename='viz/wc_cnn.png')
+word_cloud(df_foxnews['text'], filename='viz/wc_foxnews.png')
+word_cloud(df['text'], filename='viz/wc_cnn_foxnews.png')
