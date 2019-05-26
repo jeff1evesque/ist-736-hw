f55018f13e81f123ab36e48128fee0c0c1378012
jeffrey levesque <jeff1evesque@yahoo.com>
2019-05-26 12:38:10 -0400

    #129: app.py, use twitter api to collect data

--- hw7/app.py
+++ hw7/app.py
@@ -11,48 +11,73 @@ import re
 from pathlib import Path
 import pandas as pd
 import numpy as np
-import sys
-sys.path.append('..')
+from config import twitter_api as t_creds
+from consumer.twitter_query import TwitterQuery
 from view.exploratory import explore
 from view.classifier import plot_bar
 from exploratory.sentiment import Sentiment
 from datetime import datetime
 from controller.classifier import classify
 import matplotlib.pyplot as plt
+import sys
+sys.path.append('..')
 
 #
-# read dataset
+# local variables
 #
-df = pd.read_csv('{}/data/380000-lyrics-from-metrolyrics/lyrics1.csv'.format(
-    Path(__file__).resolve().parents[1]
-))
-df = df.groupby('genre').apply(lambda x: x.sample(500))
+data = {}
+screen_name = [
+    'GameofThrones',
+    'GameofThronesFinale',
+    'GOTFinale'
+]
+stopwords=[
+    'http',
+    'https',
+    'nhttps',
+    'RT',
+    'amp',
+    'co',
+    'TheStreet'
+]
+stopwords.extend(screen_name)
+
+#
+# create directories
+#
+if not os.path.exists('data/twitter'):
+    os.makedirs('data/twitter')
 
 if not os.path.exists('viz'):
     os.makedirs('viz')
 
-#
-# classify
-#
-base_results = classify(
-    df,
-    key_class='genre',
-    key_text='lyrics',
-    directory='viz',
-    top_words=25
+# instantiate api
+t = TwitterQuery(
+    t_creds['CONSUMER_KEY'],
+    t_creds['CONSUMER_SECRET']
 )
 
 #
-# classify: with ngrams
+# classify
 #
-ngram_results = classify(
-    df,
-    key_class='genre',
-    key_text='lyrics',
-    directory='viz',
-    top_words=25,
-    ngram=(1,3),
-    flag_mnb_pos=False,
-    flag_bnb_pos=False,
-    flag_svm_pos=False,
-)
+for i,sn in enumerate(screen_name):
+    #
+    # create directories
+    #
+    if not os.path.exists('viz/{sn}'.format(sn=sn)):
+        os.makedirs('viz/{sn}'.format(sn=sn))
+
+    #
+    # harvest tweets
+    #
+    if Path('data/twitter/{sn}.csv'.format(sn=sn)).is_file():
+        data[sn] = pd.read_csv('data/twitter/{sn}.csv'.format(sn=sn))
+
+    else:
+        try:
+            data[sn] = t.query(query=sn, count=600, rate_limit=900)
+            data[sn].to_csv('data/twitter/{sn}.csv'.format(sn=sn))
+
+        except Exception as e:
+            print('Error: did not finish \'{sn}\'.'.format(sn=sn))
+            print(e)
